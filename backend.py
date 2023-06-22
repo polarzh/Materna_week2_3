e1ab184b1405b7d1663651b2d4b82d725015b969
Polina <88391264+polarzh@users.noreply.github.com>
2023-06-22 18:36:45 +0300

    Update backend.py

--- backend.py
+++ backend.py
@@ -11,7 +11,7 @@ def add_census_data():
     census_data = request.files[''].stream.read().decode("utf-8")
     global df_census_data
     df_census_data = pd.read_csv(StringIO(census_data))
-    print(df_census_data[:3])
+    # print(df_census_data[:3])
 
     # merge_census_and_states_data()
     return Response(status=200)
@@ -21,8 +21,10 @@ def add_states_data():
     states_data = request.files[''].stream.read().decode("utf-8")
     global df_states_data
     df_states_data = pd.read_csv(StringIO(states_data), sep = '\t', names = ['Name', 'Lat', 'Lon'])
-    # df_states_data.loc['Name'] =  df_states_data.loc['Name'].split(',', expand = True)[0]
+    df_states_data.loc[:, 'Name'] = df_states_data['Name'].str.split(',', expand=True).drop([1], axis=1)
     print(df_states_data)
+    df_states_data = df_states_data.rename({'Name': 'State'}, axis=1)
+
     # merge_census_and_states_data()
     return Response(status=200)
 
@@ -31,12 +33,12 @@ def merge_census_and_states_data(attribute):
     print(df_states_data, df_census_data)
     print(attribute, type(attribute))
 
-    df_merged_census_states =  df_states_data.merge(df_census_data)
+    df_merged_census_states =  df_census_data.merge(df_states_data, how='left', on='State')
     print(df_merged_census_states)
-    if df_census_data.empty() or df_states_data.empty():
+    if df_census_data.empty or df_states_data.empty:
         return 'Files werent added to the backend'
     else:
-        return send_file(df_merged_census_states[attribute])
+        return Response(df_merged_census_states[attribute].to_csv())
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
